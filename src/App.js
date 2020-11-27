c7f413cafcebdc3dddacbbddf3664dccf00aef33
Dilan Londono <Dijheylon12@gmail.com>
2020-11-27 15:35:27 -0500

    Agregar play de react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Boton from './Play';
 
 function App() {
   return (
@@ -7,16 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1><code>Bienvendos al proyecto</code> Among Us.</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Component'>
+          <Boton/>
+        </div>
+        
       </header>
     </div>
   );
---
+++ src/Play.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Boton extends React.Component{
+  render(){
+    return ( 
+    //<Button color="primary"><p>Play</p></Button>
+    <h2><a href="">Play</a></h2>
+    );
+  }
+}
+
+export default Boton;
